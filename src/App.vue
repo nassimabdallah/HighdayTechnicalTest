2c3fe0e58f146a56834be7683f06ebeaf464aaeb
nassimabdallah <nassim.abdallah@live.fr>
2023-07-16 17:37:08 +0200

    Add header component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    
+  <div class="container is-fullscreen bg-main">
+    <Header />
   </div>
 </template>
 
 <script setup>
-
+import Header from './components/Header.vue';
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <header class="is-mobile is-flex is-justify-content-space-between is-align-items-center p-2">
+        <div>
+            <button class="button is-transparent">
+                <span class="icon">
+                    <i class="fa-solid fa-arrow-left"></i>
+                </span>
+            </button>
+        </div>
+        <div class="logo is-flex">
+            <a>
+                <i class="highday-logo"></i>
+            </a>
+        </div>
+        <div class="">
+            User
+        </div>
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+header .logo {
+    border: 1px solid hsl(0, 0%, 86%);
+    border-radius: 50%;
+    padding: 10px;
+}
+</style>
